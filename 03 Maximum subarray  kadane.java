8d625340381bc597f4ec937feb12dfc684ccf3c7
WarisAmir <warisamir1918@gmail.com>
2023-06-13 16:12:52 +0530

    Create 03 Maximum subarray  kadane.java

---
+++ 03 Maximum subarray  kadane.java
@@ -0,0 +1,11 @@
+// using kadane algo 
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int mx=0,currmx=nums[0];
+        for(int k:nums){
+            mx=Math.max(mx+k,k);
+            currmx=Math.max(currmx,mx);
+        }
+        return currmx;
+    }
+}
