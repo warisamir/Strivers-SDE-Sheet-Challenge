1bb82222ce22b23e79dc7e57e3bdacc9866940b1
WarisAmir <warisamir1918@gmail.com>
2023-06-13 18:13:31 +0530

    Create Merge two linkedlist.java

---
+++ day 5/Merge two linkedlist.java
@@ -0,0 +1,22 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode head1, ListNode head2) {
+     
+     ListNode dummy= new ListNode(-1);
+     ListNode head=dummy,tail=dummy;
+     while(head1!=null && head2!=null){
+         if(head1.val <head2.val){
+             tail.next=head1;
+             head1=head1.next;
+             }
+             else{
+                 tail.next=head2;
+                 head2=head2.next;
+             }
+             tail=tail.next;
+     }
+     if(head1!=null) tail.next=head1;
+     else tail.next=head2;
+     return dummy.next;
+     
+    }
+}
